1debebfaf9262c0eeed9a7695ab64b9cdd5edfc0
Mickey  Le <thanhdat2902@gmail.com>
2018-07-03 00:36:01 +0200

    remote backend

--- src/app/participant.service.ts
+++ src/app/participant.service.ts
@@ -16,8 +16,8 @@ const httpOptions = {
 export class ParticipantService {
 
   //private URL = 'http://localhost:3000/api';  // URL to local api
-  private URL = 'http://192.168.2.106:3000/api'; 
-  //private URL = 'https://ghi-danh-server.herokuapp.com/api';  // URL to web api
+  //private URL = 'http://192.168.2.106:3000/api'; 
+  private URL = 'https://ghi-danh-server.herokuapp.com/api';  // URL to web api
   
   constructor(
     private http: HttpClient,
